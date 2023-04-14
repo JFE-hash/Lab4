09251cea6e68f73c701ec17cb3fbcef698bffccf
JFE-hash <egorikmenya@gmail.com>
2023-04-15 00:38:14 +0300

    done

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 nums = [1, 228, 1337, 1941, 10]
 ans = int(input(print('Загадайте число:')))
+print('Исходный список:', *nums)
+print('Ваше число:', ans)
 if ans in nums:
     print('Поздравляю, Вы угадали число!')
 else:
